99dabf3ebe71be5593b6f5d4eb4e9a8495761190
Feraz Khan <ferazahmadgit62@gmail.com>
2023-10-04 10:35:17 +0530

    password hashed

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -1,8 +1,15 @@
 const { User } = require("../models/user");
+const bcrypt = require("bcrypt");
 exports.fetchAlluser = async (req, res, next) => {
   const allUsers = await User.findAll();
   console.log(allUsers);
 };
 exports.createUser = async (req, res, next) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
+    const hashed = await bcrypt.hash(password, 12);
+    const user = await User.create({ name, email, password: hashed });
+  } catch (error) {
+    console.log(error);
+  }
 };
